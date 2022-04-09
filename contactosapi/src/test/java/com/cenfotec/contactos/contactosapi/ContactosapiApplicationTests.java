f9b61acb8647ced218541e01fcf846e8e0915cdf
jcruzg <xerojcg@gmail.com>
2022-04-08 21:55:00 -0600

    Commit lab 6

---
+++ contactosapi/src/test/java/com/cenfotec/contactos/contactosapi/ContactosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cenfotec.contactos.contactosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
